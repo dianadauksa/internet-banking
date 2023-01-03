b710bd38d8016532fe816327e8ea2f44936c4b40
Diana <dianadauksa@gmail.com>
2023-01-03 16:02:30 +0200

    add accounts view partials

--- resources/views/accounts/show.blade.php
+++ resources/views/accounts/show.blade.php
@@ -6,17 +6,29 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <h2>{{ Auth::user()->firstName  }}, you can open new or delete your bank accounts here.</h2>
+
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ Auth::user()->firstName  }}, you can open new, edit the name of, or delete your accounts here.
-                    <br>
                     @foreach ($bankAccounts as $bankAccount)
                         Account name: {{ $bankAccount->name }}
                         Account number: {{ $bankAccount->account_number }}
                         Currency: {{ $bankAccount->currency }}
                         Money available: {{ $bankAccount->balance }} {{ $bankAccount->currency }}
                     @endforeach
+                </div>
+            </div>
+
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="max-w-xl">
+                    @include('accounts.partials.create-account-form')
+                </div>
+            </div>
+
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="max-w-xl">
+                    @include('accounts.partials.delete-account-form')
                 </div>
             </div>
         </div>
