196d51d8991bdf6eef05e4e78fb8e0e5a8c20582
Diana <dianadauksa@gmail.com>
2023-01-04 09:51:40 +0200

    Update BankAccountMiddleware.php

--- app/Http/Middleware/BankAccountMiddleware.php
+++ app/Http/Middleware/BankAccountMiddleware.php
@@ -3,13 +3,12 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use App\Models\BankAccount;
 
 class BankAccountMiddleware
 {
     public function handle($request, Closure $next)
     {
-        $bankAccounts = BankAccount::where('user_id', auth()->id())->get();
+        $bankAccounts = auth()->user()->bankAccounts()->get();
         view()->share('bankAccounts', $bankAccounts);
 
         return $next($request);
