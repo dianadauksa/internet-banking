cf54793c742b025dcb0f0473a8e77e6439f3a945
Diana <dianadauksa@gmail.com>
2023-01-07 16:10:07 +0200

    update statement filtering

--- app/Http/Controllers/StatementController.php
+++ app/Http/Controllers/StatementController.php
@@ -16,7 +16,56 @@ class StatementController extends Controller
 
     public function show(Account $account): View
     {
-        $transactions = auth()->user()->transactions()->where('account_from_id', $account->id)->orWhere('account_to_id', $account->id)->get();
+        if ($account->user_id !== auth()->user()->id) {
+            return abort('403');
+        }
+
+        $transactions = $account->transactions()->get();
+        $transactions = $transactions->sortByDesc('created_at');
+        return view('statements.single', ['transactions' => $transactions, 'account' => $account]);
+    }
+
+    public function filter(Request $request, Account $account): View
+    {
+        $transactions = $account->transactions()->get();
+
+        if ($request->account_number) {
+            $transactions = $transactions->filter(function ($transaction) use ($request) {
+                return ($transaction->getAccountFrom->number === $request->account_number
+                    || $transaction->getAccountTo->number === $request->account_number);
+            });
+        }
+
+        if ($request->sender) {
+            $transactions = $transactions->filter(function ($transaction) use ($request) {
+                if (str_contains($transaction->getAccountFrom->user->getFulLName(), $request->sender)) {
+                    return $transaction;
+                }
+                return null;
+            });
+        }
+
+        if ($request->recipient) {
+            $transactions = $transactions->filter(function ($transaction) use ($request) {
+                if (str_contains($transaction->getAccountTo->user->getFulLName(), $request->recipient)) {
+                    return $transaction;
+                }
+                return null;
+            });
+        }
+
+        if ($request->form || $request->to) {
+            $transactions = $transactions->filter(function ($transaction) use ($request) {
+                if ($request->from && $request->to) {
+                    return ($transaction->created_at->format('Y-m-d') >= $request->from
+                        && $transaction->created_at->format('Y-m-d') <= $request->to);
+                } elseif ($request->from) {
+                    return ($transaction->created_at->format('Y-m-d') >= $request->from);
+                }
+                return ($transaction->created_at->format('Y-m-d') <= $request->to);
+            });
+        }
+
         $transactions = $transactions->sortByDesc('created_at');
         return view('statements.single', ['transactions' => $transactions, 'account' => $account]);
     }
