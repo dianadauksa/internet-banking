27996fd77a7f16ed082e040b2b597db492599217
Diana <dianadauksa@gmail.com>
2023-01-15 20:42:44 +0200

    Update main.blade.php

--- resources/views/crypto/main.blade.php
+++ resources/views/crypto/main.blade.php
@@ -22,7 +22,7 @@
                     <div class="bg-white overflow-hidden shadow sm:rounded-lg">
 
                         <div class="p-6 border-b border-gray-200">
-                            <a href="{{ route('accounts.show', $account) }}" class="block font-bold text-xl mb-2 account-name">
+                            <a href="{{ route('crypto') }}" class="block font-bold text-xl mb-2 account-name">
                                 {{ $account->name }}
                             </a>
                             <div class="text-gray-700 font-medium text-sm">
