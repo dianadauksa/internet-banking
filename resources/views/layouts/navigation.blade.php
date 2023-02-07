58100ab699c5d681c3df769fa87e5cdd89e8e02e
Diana <dianadauksa@gmail.com>
2023-02-07 16:06:00 +0200

    Update navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -55,7 +55,7 @@
 
                     <x-slot name="content">
                         <x-dropdown-link :href="route('profile.edit')">
-                            {{ __('Profile') }}
+                            {{ __('Profile & Codes') }}
                         </x-dropdown-link>
 
                         <!-- Authentication -->
