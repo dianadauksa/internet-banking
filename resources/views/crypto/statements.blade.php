1e83fc0412862c0ea6fe48660938b4b3cccd3138
Diana <dianadauksa@gmail.com>
2023-02-08 17:20:26 +0200

    Update statements.blade.php

--- resources/views/crypto/statements.blade.php
+++ resources/views/crypto/statements.blade.php
@@ -11,6 +11,7 @@
     </x-slot>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+
             <section class="space-y-6">
                 <div class="bg-white overflow-hidden shadow sm:rounded-lg">
 
@@ -27,6 +28,22 @@
                     </div>
 
                 </div>
+            </section>
+            <section>
+                <form method="post" action="">
+                    @csrf
+
+                    <div class="flex items-center space-x-4 py-2 border-b border-gray-300">
+                        <input type="text" name="coin" placeholder="Search by coin, e.g. BNB" class="form-input w-52 rounded-md shadow-sm mr-2" value="{{ request('coin') }}">
+                        <span class="px-2">From</span><input type="date" name="from" class="form-input w-44 rounded-md shadow-sm mr-2" value="{{ request('from') }}">
+                        <span class="px-2">To</span><input type="date" name="to" class="form-input w-44 rounded-md shadow-sm mr-1" value="{{ request('to') }}">
+                        <label for="buy" class="px-1">Buy</label><input type="radio" id="buy" name="type" value="buy">
+                        <label for="sell" class="px-1">Sell</label><input type="radio" id="sell" name="type" value="sell">
+                        <label for="shortlist" class="px-1">Shortlist</label><input type="radio" id="shortlist" name="type" value="shortlist">
+                        <x-primary-button class="ml-2">Search transactions</x-primary-button>
+                    </div>
+                </form>
+
             </section>
 
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
